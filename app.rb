a6c68ba9233e0fdab175470150d46c97799bd003
Arian Ryan <arian@beenverified.com>
2011-07-21 16:45:21 -0400

    added sinbook stuff

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require 'sinatra'
 require 'sinbook'
 
 class App < Sinatra::Application
+  
+  facebook do
+    api_key  '119118001500358'
+    secret   'b140473706e8567150a196cccb776886'
+    app_id   119118001500358
+    url      'http://apps.facebook.com/NumberGuru'
+    callback 'http://174.122.43.76/fb'
+  end
+    
   get '/' do
     'It still worked, Ronin! Or does it?'
   end
+  
+  get '/fb' do
+    fb.require_login!
+    "Hi <fb:name uid=#{fb[:user]} useyou=false />!"
+  end
+  
 end

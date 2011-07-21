6f4dfe98edfaae53a75a0d537f3df2b202aa65a6
Arian Ryan <arian@beenverified.com>
2011-07-21 16:35:39 -0400

    added deploy scripts

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -9,16 +9,9 @@ config_file=local_config/env.config
 echo "SSHing into server"
 ssh -p 1025 $user "cd $server_destination_dir/$folder
 
-# echo stashing any local changes
-# git stash
 echo fetching latest source code
 git fetch origin
 git rebase origin master
-# git stash apply
-
-# echo setting environment configuration
-# rm $config_file
-# bash -c 'echo -n staging > $config_file'
 
 echo hupping unicorn
 ls -l local_config/unicorn_hup
